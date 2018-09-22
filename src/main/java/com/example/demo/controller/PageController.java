fc22d166d941e0ecd8a8aa594508f7a4cf00a35d
kaelky <rickychandra.j@gmail.com>
2018-09-22 20:40:18 +0800

    Finishing WriteUp Question

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -2,8 +2,12 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Controller
 public class PageController {
@@ -12,9 +16,20 @@ public class PageController {
         return "viral";
     }
 
-    @RequestMapping("/challenge")
+    /*@RequestMapping("/challenge")
     public String challenge(@RequestParam(value = "name") String name, Model model){
         model.addAttribute("name", name);
+        return "challenge";
+    }*/
+
+    @RequestMapping(value = {"/challenge", "challenge/{name}"})
+    public String challengePath(@PathVariable Optional<String> name, Model model){
+        if (name.isPresent()){
+            model.addAttribute("name", name.get());
+        }
+        else {
+            model.addAttribute("name", "KB");
+        }
         return "challenge";
     }
 }
